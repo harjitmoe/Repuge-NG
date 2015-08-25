d378739f9e517690a6ae0bad5f45ca8d5fe984f7
Thomas Hori <thomas.hori@liddicott.com>
2015-08-25 15:32:22 +0100

    Immobilise collectones because performance. Remove older maze generator (if I reintroduce it, I intend to rewrite it.  Purging older code is an aim here, albeit unlikely that KSP would put in a copyright claim.)
    Signed-off-by: Thomas Hori <thomas.hori@liddicott.com>

--- CollectoObject.py
+++ CollectoObject.py
@@ -13,40 +13,6 @@ class CollectoObject(GridObject):
         """Just been spawned.  Do what?"""
         self.add_handler(1,self.onetick)
     #
-    def onetick(self):
-        if self.nan==self.nan and hasattr(self.level,"nan"):
-            self.nan=self.level.nan
-        for i in range(5):
-            type_=random.randrange(5)
-            if type_==0: target=(self.pt[0],self.pt[1]+1)
-            if type_==1: target=(self.pt[0]+1,self.pt[1])
-            if type_==2: target=(self.pt[0],self.pt[1]-1)
-            if type_==3: target=(self.pt[0]-1,self.pt[1])
-            if type_==4: target=(self.pt[0],self.pt[1])
-            try: #XXX kludge/fragile/assumes
-                floorlevel=type(0)(self.level.get_index_grid(*self.pt)[0][5:])
-            except ValueError:
-                floorlevel=1 #Needed or mazed subclass breaks
-            nxtstat=self.level.get_index_grid(*target)[0]
-            if self.level.objgrid[target[0]][target[1]]:
-                breakp=1
-                for obj in self.level.objgrid[target[0]][target[1]][:]:
-                    if hasattr(obj,"myinterface") and obj.myinterface!=None:
-                        self.handle_contact(obj)
-                else:
-                    breakp=0
-                if breakp:
-                    break
-            elif nxtstat.startswith("floor"):
-                newlevel=type(0)(nxtstat[5:])
-                if (newlevel-floorlevel)<=1:
-                    break
-            type_+=1
-            type_%=5
-        else: #i.e. ran to completion with no break
-            return #stuck, cannot move
-        self.place(*target)
-    #
     def handle_contact(self,playerobj):
         self.level.redraw()
         self.game.bug_report[__name__]={}
