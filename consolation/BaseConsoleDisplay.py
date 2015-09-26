fc478c1cdd3364eb24f1f40e35088666ea9d1d11
Thomas Hori <thomas.hori@liddicott.com>
2015-09-26 20:52:03 +0100

    Purge more KSP code.  And get rid of the atiquated and kludgy 3-message buffer in use since the beginning due to the -- More -- support being hacked on rather late (it is properly integrated in -NG anyway).
    Signed-off-by: Thomas Hori <thomas.hori@liddicott.com>

--- consolation/BaseConsoleDisplay.py
+++ consolation/BaseConsoleDisplay.py
@@ -13,7 +13,6 @@ class BaseConsoleDisplay(BaseDisplay):
     """
     _tiles_class = ConsoleTiles
     def __init__(self, *a, **kw):
-        self._messages_visible = ["", "", ""]
         self.point = [0, 0]
         self.mylock=thread.allocate_lock()
         super(BaseConsoleDisplay, self).__init__(*a, **kw)
@@ -30,7 +29,7 @@ class BaseConsoleDisplay(BaseDisplay):
         if self._message_queue:
             while self._message_queue[1:]:
                 line = self._message_queue.pop(0)
-                self._put_to_message_area(line+" -- More -- ", 1, line, 0)
+                self._put_to_message_area(line+" -- More -- ", 1)
             self._put_to_message_area(self._message_queue.pop(), 0)
     def ask_question(self, question):
         self.dump_messages()
@@ -42,9 +41,7 @@ class BaseConsoleDisplay(BaseDisplay):
     #
     def _plot_character(self, y_coord, x_coord, character):
         raise NotImplementedError("should be implemented by subclass")
-    def _put_to_message_area(self, s, ask, s2=None, collect_input=1):
-        #Note: this function may be partially copyrighted by KSP.
-        #To be possibly rewritten.
+    def _put_to_message_area(self, s, ask):
         """The backend behind all putting to the console message area,
         for ask or say.
 
@@ -52,51 +49,25 @@ class BaseConsoleDisplay(BaseDisplay):
 
         Arguments:
         - s: The string to output.
-        - ask: Boolean, should user input be collected?
-        - s2: String for in-place change of question after user input collected.
-          This should ONLY be used for removing -- More -- prompts and the like,
-          which should not be kept around in the message log.
+        - ask: Boolean, should user input be waited for?
         - collect_input: Boolean, should the user input be collected?  Keep as 1
           unless the user is supposed to acknowledge receipt of the message with
           Return but not actually supposed to input stuff (e.g. More prompt).
         """
-        if s2 == None:
-            s2 = s
-        self._engage_message_formatting()
-        self._messages_visible.pop(0)
-        old_point = self.point[:]
-        returndat = None
         _w = self.get_dimensions()[0]
         if _w < 0:
             _w = 80
-        _w -= 1
+        s_padded = s
+        if len(s) < (_w-1):
+            s_padded += (_w-len(s)-1)*" "
+        self._engage_message_formatting()
+        save_point = self.point[:]
+        retu = None
+        self.goto_point(0,21)
+        self._output_text(s_padded)
         if ask:
-            self.goto_point(0, 19)
-            for i in self._messages_visible:
-                self._output_text(i+"\n")
-            self._output_text(" "*_w+"\n")
-            self.goto_point(0, 21)
             self._reset_terminal()
-            if not collect_input:
-                self._output_text(s)
-                #Wait for key event without triggering recursion.
-                self.mylock.acquire()
-                bkq, self._message_queue = self._message_queue, []
-                self.get_key_event()
-                self._message_queue = bkq+self._message_queue
-                self.mylock.release()
-                s = s2
-            else:
-                returndat = Compat3k.prompt_user(s)
-                s = s2
-                s = s+returndat
-        while len(s) < _w:
-            s += " "
-        self._messages_visible.append(s)
-        self.goto_point(0, 19)
-        for i in self._messages_visible:
-            self._output_text(i+"\n")
+            retu = Compat3k.prompt_user("\r"+s)
         self._end_message_formatting()
-        self.goto_point(*old_point)
-        if collect_input:
-            return returndat
+        self.goto_point(*save_point)
+        return retu
